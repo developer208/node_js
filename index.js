23b83bfc609980d53533eec51561f897fb85ce3b
vedang <it.developer2002@gmail.com>
2022-06-08 14:47:09 +0530

    initiallizing node js repo

---
+++ index.js
@@ -0,0 +1,9 @@
+const app=require('./app')
+console.log("hello");
+var a=30;
+const b=40;
+console.log(a+b);
+console.log(app.z());
+
+// interview question 
+// console.log() provide by browser and node are totally different.
