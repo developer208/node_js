f0b73fcfe54f5ef0dbaf2162882cec2389bd247e
vedang <it.developer2002@gmail.com>
2022-06-08 18:38:02 +0530

    deleted unused  js files

--- filter.js
+++
@@ -1,8 +0,0 @@
-const arr=[2,4,6,5,1,3,7,3,3];
-// interview question
-// The filter() method creates a new array with all elements that pass the test implemented by the provided function. 
-let result=arr.filter((item)=>{
-    return item>3;
-
-})
-console.warn(result);
--- module.js
+++
@@ -1,7 +0,0 @@
-const gs=require('fs').writeFileSync; 
-console.log("vedang mule");
-gs("xyz.txt","lazy_coder208");
-console.log("=>>",__dirname);
-console.log("=>>",__filename);
-
-// fs is non global module.(file system).
